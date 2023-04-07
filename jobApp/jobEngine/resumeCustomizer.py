224e78a040bafd807317e836668c823b950e6f06
samihomiebro <samihomiebro@gmail.com>
2023-04-07 17:50:09 +0200

    added better desc

--- jobApp/jobEngine/resumeCustomizer.py
+++ jobApp/jobEngine/resumeCustomizer.py
@@ -8,28 +8,28 @@ from resumeParser import Resume, canvas
 class ResumeBuilder:
     def __init__(self, resume_path, job=None):
         self.old_resume_content = Resume(resume_path).extract_text()
-        self.job_description = job.job_description
-
     
     def generateCustomResumeAi(self, job:Job) ->str:
         self.job_description = job.job_description
         self.chatgpt = ChatGPT("jobApp/secrets/openai.json")
-        query = f"Rewrite my resume and tailor it to the job description below. \
+        query = f"rewrite my resume by adding the required skills from the following job desciption to become suitable candidate for the job. \
         Here is my resume: {self.old_resume_content}. Here is the job description: {self.job_description}."
         new_resume = self.chatgpt.ask(query)
         print(new_resume)
         return new_resume
     
-    def createCustomResumePdf(self, job:Job, new_resume_path:str) -> None:
+    def createCustomResumePdf(self, job:Job, new_resume_path=None) -> None:
         """ create an ai customized resume and save it as pdf
             if new path is left blank,  write to jobApp/data/cv_name_last_jobTitle_comapny.pdf
         """
     #@TODO add applicant class
-        name = "alex"
-        last = "red"
+        name = "zayneb"
+        last = "dhieb"
         if new_resume_path == None:
-            new_resume_path = f"jobApp/data/cv_{name}_{last}_{job.job_title}_{job.company_name}.pdf"
-        Resume.saveContentToPdf(self.generateCustomResumeAi(job), new_resume_path)
+            new_resume_path = f"jobApp/data/cv_{name}_{last}_{job.job_title}_{job.company_name}.txt"
+        with open(new_resume_path, "w") as f:
+            f.write(self.generateCustomResumeAi(job))
+        #Resume.saveContentToPdf(self.generateCustomResumeAi(job), new_resume_path)
 
 
 if __name__ == '__main__':
@@ -41,5 +41,6 @@ if __name__ == '__main__':
     CvCustomizer = ResumeBuilder(
             'jobApp/data/zayneb_dhieb_resume_english.pdf')
     for job in jobs:
-        customCv = CvCustomizer.generateCustomResumeAi(
-            'jobApp/data/zayneb_dhieb_resume_english.pdf', job)
+        #customCv = CvCustomizer.generateCustomResumeAi(job)
+        CvCustomizer.createCustomResumePdf(job)
+        break

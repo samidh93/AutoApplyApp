9982da69bff80dc1a1cc526cde507b6491719e44
samihomiebro <samihomiebro@gmail.com>
2023-05-14 21:34:21 +0200

    updated job parser linkedin to get onsite and offsite links in the same csv

--- jobApp/jobEngine/jobParserLinkedin.py
+++ jobApp/jobEngine/jobParserLinkedin.py
@@ -8,6 +8,8 @@ import re
 from linkedinEasyApplyLegacyCode import EasyApplyLinkedin
 from fileLocker import FileLocker
 import time
+
+
 class JobParser:
     def __init__(self, linkedin_data, csv_links='jobApp/data/links.csv'):
         """Parameter initialization"""
@@ -15,42 +17,48 @@ class JobParser:
             data = json.load(config_file)
         self.base_url = data["urls"]['search_job_url']
         self.page_num = data["params"]['pageNum']
-        self.job_title= data["login"]['keywords']
+        self.job_title = data["login"]['keywords']
         self.location = data["login"]['location']
         self.job_pos = data["params"]['start']
         self.filter_easy_apply = data["params"]['f_AL']
         self.params = {
             'keywords': self.job_title,
             'location': self.location,
-            'position': self.job_pos, # 25 per page
-            'pageNum': self.page_num, # we increment this for next page
-            'f_AL': self.filter_easy_apply # we increment this for next page
+            'position': self.job_pos,  # 25 per page
+            'pageNum': self.page_num,  # we increment this for next page
+            'f_AL': self.filter_easy_apply  # we increment this for next page
         }
         self.jobList = []
         self.easyApplyList = []
         self.offsiteApplyList = []
         self.officialJobLinks = []
         # the bot
-        self.bot = EasyApplyLinkedin('jobApp/secrets/linkedin.json', headless=True)
+        self.bot = EasyApplyLinkedin(
+            'jobApp/secrets/linkedin.json', headless=True)
         self.csv_file = csv_links
+        # pair to store links {"onsite": None, "offsite": None}
+        self.links_pair = {"onsite": "None", "offsite": "None"}
+        # list of dict [ {"onsite": None, "offsite": None} ]
+        self.links_pair_list = []
 
     def setEasyApplyFilter(self, easy_apply_filter=False):
         print("Warning: easy apply filter is only visible for logged user ")
         self.filter_easy_apply = easy_apply_filter
         # set the filter to true or false
-        self.params['f_AL']=easy_apply_filter
+        self.params['f_AL'] = easy_apply_filter
 
-    def generateLinksPerPage(self, max_pages = 5)-> list: #125 jobs
+    def generateLinksPerPage(self, max_pages=5) -> list:  # 125 jobs
         for _ in range(max_pages):
             self.generateLinks()
-        #@NOTE: added save to csv to keep track of links
+        # @NOTE: added save to csv to keep track of links
         if self.filter_easy_apply:
-            self.saveLinksToCsv(links= [self.easyApplyList, "na"])
+            self.saveLinksToCsv(links=[self.easyApplyList, "na"])
             return [self.easyApplyList, "na"]
         else:
-            self.saveLinksToCsv(links= [self.offsiteApplyList, self.officialJobLinks ])
-            return [self.offsiteApplyList, self.officialJobLinks ]
-        
+            self.saveLinksToCsv(
+                links=[self.offsiteApplyList, self.officialJobLinks])
+            return [self.offsiteApplyList, self.officialJobLinks]
+
     def generateLinks(self):
         full_url = f"{self.base_url}?{'&'.join([f'{k}={v}' for k, v in self.params.items()])}"
         print(f"constructed url: {full_url }")
@@ -67,7 +75,8 @@ class JobParser:
         else:
             print("jobCount not found")
         # Find the element with the id 'results-list__title'
-        element = soup.select_one('#main-content > section.two-pane-serp-page__results-list > ul')
+        element = soup.select_one(
+            '#main-content > section.two-pane-serp-page__results-list > ul')
         # Find all <li> elements
         li_elements = element.find_all('li')
         links = []
@@ -75,94 +84,91 @@ class JobParser:
             if li.find("a") and "href" in li.a.attrs:
                 href = li.a["href"]
                 print("link to job found: "+str(href))
-                if self.filter_easy_apply: # if we are looking only for easy apply jobs
+                if self.filter_easy_apply:  # if we are looking only for easy apply jobs
                     self.filterJobList(href, True, False)
                     links.append(href)  # keep track of global links
                 else:
                     self.filterJobList(href, False, True)
-                    links.append(href)              
-        self.jobList+= links
+                    links.append(href)
+        self.jobList += links
         print(f"total links found {len(self.jobList)}")
         if self.filter_easy_apply:
-            print(f"total links easy apply found {len(self.easyApplyList)}")  
-            return  [self.easyApplyList, "na"]
+            print(f"total links easy apply found {len(self.easyApplyList)}")
+            return [self.easyApplyList, "na"]
         else:
-            print(f"total links offsite apply found {len(self.offsiteApplyList)}") 
-            return [self.offsiteApplyList, self.officialJobLinks ]
-        
+            print(
+                f"total links offsite apply found {len(self.offsiteApplyList)}")
+            return [self.offsiteApplyList, self.officialJobLinks]
+
     def generateLinksSeleniumV2(self, pages=5):
         self.bot.getEasyApplyJobSearchUrlResults()
-        self.jobList = self.bot.getUnlockJobLinksNoLogin(pages) # we have all links
+        self.jobList = self.bot.getUnlockJobLinksNoLogin(
+            pages)  # we have all links
         for i, link in enumerate(self.jobList):
-            #if self.filter_easy_apply: # if we are looking only for easy apply jobs
-            #    self.filterJobList(link, True, False)
-            #else:
-            #    self.filterJobList(link, False, True)
-                print(f"filtering job at: {i}")
-                self.filterJobList(link, True, True)
+            print(f"filtering job with index {i}")
+            pair = self.filterJobList(link)
+            print(f"pair: {pair}")
+            self.links_pair_list.append(pair)
+        print(f"list: {self.links_pair_list}")
+        self.saveLinksToCsv(self.links_pair_list, self.csv_file)
 
-        # save all in same csv file: testing purpose
-        print(f"total links intern (easy apply) found {len(self.easyApplyList)}")  
-        print(f"total links extern (official apply) found {len(self.officialJobLinks)}")  
-        self.saveLinksToCsv( [self.easyApplyList, self.officialJobLinks], self.csv_file)
-        return  [self.easyApplyList,self.officialJobLinks]
-        #if self.filter_easy_apply:
-        #    print(f"total links easy apply found {len(self.easyApplyList)}")  
-        #    self.saveLinksToCsv( [self.easyApplyList, "na"], self.csv_file)
-        #    return  [self.easyApplyList, "na"]
-        #else:
-        #    print(f"total links offsite apply found {len(self.offsiteApplyList)}") 
-        #    self.saveLinksToCsv( [self.offsiteApplyList, self.officialJobLinks ],self.csv_file)
-        #    return [self.offsiteApplyList, self.officialJobLinks ]
+    def filterJobList(self, job_href, onsite=False, offsite=False) -> dict:
+        links_pair = {"onsite": "None", "offsite": "None"}
+        response = requests.get(job_href)
+        html_source = response.content
+        # @NOTE: the requests here to be moved while getting the url at the same step
+        time.sleep(1)  # slow down request
+        # Create a BeautifulSoup object to parse the HTML source code
+        soup = BeautifulSoup(html_source, "html.parser")
+        # find offsite apply
+        button = soup.find('button', {
+                           'data-tracking-control-name': 'public_jobs_apply-link-offsite_sign-up-modal'})
+        # public_jobs_apply-link-onsite
+        if button is not None:  # if found
+            print("offsite apply button found")
+            # self.offsiteApplyList.append(job_href)
+            offsite_link = self.getOfficialJobLink(soupObjRef=soup)
+            # {"onsite": None, "offsite": None}
+            links_pair["offsite"] = offsite_link
+            # {"onsite": None, "offsite": None}
+            links_pair["onsite"] = "None"
+        else:
+            print("button offsite not found, onsite apply is ommited")
+            # {"onsite": None, "offsite": None}
+            links_pair["offsite"] = "None"
+            # {"onsite": None, "offsite": None}
+            links_pair["onsite"] = job_href
+        return links_pair
 
-    def filterJobList(self, job_href, onsite=False, offsite=False )-> list:
-            try:
-                response = requests.get(job_href)
-                html_source = response.content
-                time.sleep(1)
-                # Create a BeautifulSoup object to parse the HTML source code
-                soup = BeautifulSoup(html_source, "html.parser")
-                if offsite:
-                    button = soup.find('button', {'data-tracking-control-name': 'public_jobs_apply-link-offsite_sign-up-modal'})
-                    if button is not None:
-                        print("offsite apply")
-                        self.offsiteApplyList.append(job_href)
-                        self.getOfficialJobLink(soupObjRef=soup)
-                        return self.offsiteApplyList
-                    else:
-                        print("button offsite not found")  
-
-                if onsite:
-                    button = soup.find('button', {'data-tracking-control-name': 'public_jobs_apply-link-onsite'})
-                    if button is not None:
-                        print("onsite apply")
-                        self.easyApplyList.append(job_href)
-                        return self.easyApplyList
-                    else:
-                        print("button onsite not found")  
-            except:
-                print("url no reachable")
-
-    def getOfficialJobLink(self, soupObjRef:BeautifulSoup):
-        #soup = BeautifulSoup(html_doc, "html.parser")
+    def getOfficialJobLink(self, soupObjRef: BeautifulSoup) -> str:
+        # soup = BeautifulSoup(html_doc, "html.parser")
         # find the element with the 'id' attribute value of 'applyUrl'
         apply_url_element = soupObjRef.find(id='applyUrl')
         # find the comment node inside the 'applyUrl' element
-        comment_node = (apply_url_element.find(string=lambda string: isinstance(string, Comment)))
+        comment_node = (apply_url_element.find(
+            string=lambda string: isinstance(string, Comment)))
         official_link = comment_node.replace('"', '')
         # print the text content of the comment node
-        print(f"official job link: {official_link}\n")
+        print(f"official job link: {official_link}")
         self.officialJobLinks.append(official_link)
-         
-    def extract_link_id(self, link:str):
+        return official_link
+
+    def extract_link_id(self, link: str):
+        if link["onsite"] == "None":
+            link = link["offsite"]
+        else:
+            link = link["onsite"]
         # find the first occurrence of "?"
         question_mark_index = link.find("?")
         # extract the substring before "?"
         id_string = link[:question_mark_index]
         # find the last occurrence of "-" before "?"
-        dash_index = id_string.rfind("-")
-        # extract the substring after the last "-"
-        id = id_string[dash_index+1:]
+        dash_index = id_string.rfind("-") 
+        if dash_index != -1:# if - found, id write
+            id = id_string[dash_index+1:]
+        else:
+            dash_index = id_string.rfind("/")
+            id = id_string[dash_index+1:]        
         return id
 
     def saveLinksToCsv(self, links, csv_file):
@@ -177,39 +183,45 @@ class JobParser:
                 reader = csv.reader(file)
                 next(reader)  # Skip header row
                 for row in reader:
-                    ids.append(row[3]) # we get all ids there
+                    ids.append(row[3])  # we get all ids there
                 flocker.unlock(file)
             # write
-            
+
             with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
                 flocker.lockForWrite(file)
                 writer = csv.writer(file)
-                for i, link in enumerate(links[0]): # new links loop
-                    if self.extract_link_id(link) not in ids: 
-                        counter +=1
-                        writer.writerow([len(ids)+counter, self.job_title, self.location, self.extract_link_id(link) , link, links[1][i]])
+                for i, link in enumerate(links):  # new links loop
+                    if self.extract_link_id(link) not in ids:
+                        counter += 1
+                        writer.writerow([len(ids)+counter,
+                                         self.job_title,
+                                         self.location,
+                                        self.extract_link_id(link),
+                                         link["onsite"],
+                                         link["offsite"]])
                 flocker.unlock(file)
         # no csv, write new from zero
-        else: 
+        else:
             with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
                 flocker.lockForWrite(file)
                 writer = csv.writer(file)
                 # Write the header row if the file is empty
                 if os.stat(csv_file).st_size == 0:
-                    writer.writerow(['id', 'keyword', 'location','link id', 'internal link', 'external link'])
-                for i, link in enumerate(links[0]):
-                    writer.writerow([i+1, 
-                                     self.job_title, 
-                                     self.location, 
+                    writer.writerow(
+                        ['id', 'keyword', 'location', 'link id', 'internal link', 'external link'])
+                for i, link in enumerate(links):
+                    writer.writerow([i+1,
+                                     self.job_title,
+                                     self.location,
                                      self.extract_link_id(link),
-                                     link, 
-                                     links[1][i]])
+                                     link["onsite"],
+                                     link["offsite"]])
                 flocker.unlock(file)
         print(f"Links saved to {csv_file}")
 
 
 if __name__ == '__main__':
 
-   jobParserObj= JobParser('jobApp/secrets/linkedin.json')
-   jobParserObj.setEasyApplyFilter(False)
-   jobsLinks = jobParserObj.generateLinksSeleniumV2()
+    jobParserObj = JobParser('jobApp/secrets/linkedin.json')
+    jobParserObj.setEasyApplyFilter(False)
+    jobsLinks = jobParserObj.generateLinksSeleniumV2()

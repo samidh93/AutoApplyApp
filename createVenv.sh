9a165f1e6ea2792f54d26f7d3dc0bc422c778ef5
samidh93 <sami.dhiab.x@gmail.com>
2025-01-21 01:10:06 +0100

    fixed missing packages

--- createVenv.sh
+++ createVenv.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 # Create the virtual environment
 python3 -m venv venv
 
 # Activate the virtual environment
-source "venv/bin/activate"
+source venv/bin/activate
 
 # Install packages from requirements.txt
 pip install -r requirements.txt
